8b75590fa444ed8cfec5f87239f60e831ad69ae0
henrikmv <henrik@devOtta.no>
2024-02-13 14:42:12 +0100

    feat: create basic chart

--- src/Plugin.tsx
+++ src/Plugin.tsx
@@ -1,24 +1,15 @@
 import React, {useState} from "react";
-import {Button} from "@dhis2/ui";
 import './Plugin.module.css';
-import { EnrollmentOverviewProps } from "./Plugin.types";
 import {WidgetCollapsible} from "./Components/WidgetCollapsible";
-import {EnrollmentViewer} from "./Components/EnrollmentViewer";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {buildUrlQueryString} from "./utils/buildURLQueryString";
+import { GrowthChart } from "./Components/GrowthChart/GrowthChartDefault";
 
 const queryClient = new QueryClient();
 
-const PluginInner = (propsFromParent: EnrollmentOverviewProps) => {
+const PluginInner = () => {
     const [open, setOpen] = useState(true);
 
-    const {
-        programId,
-        enrollmentId,
-        orgUnitId,
-        teiId,
-        navigate,
-    } = propsFromParent;
+    
 
     return (
         <QueryClientProvider
@@ -37,40 +28,14 @@ const PluginInner = (propsFromParent: EnrollmentOverviewProps) => {
                     }}
                 >
                     <WidgetCollapsible
-                        header={'Other programs'}
+                        header={'Growth Chart'}
                         borderless={false}
                         open={open}
                         onOpen={() => setOpen(true)}
                         onClose={() => setOpen(false)}
                     >
-                        <div
-                            style={{
-                                padding: '15px',
-                                paddingTop: 0,
-                                display: 'flex',
-                                flexDirection: 'column',
-                                gap: '15px',
-                            }}
-                        >
-                            <EnrollmentViewer
-                                programId={programId}
-                                enrollmentId={enrollmentId}
-                                orgUnitId={orgUnitId}
-                                teiId={teiId}
-                                navigate={navigate}
-                            />
-
-                            <div>
-                                <Button
-                                    primary
-                                    onClick={() => navigate(`enrollment?${buildUrlQueryString({
-                                        orgUnitId,
-                                        teiId,
-                                    })}`)}
-                                >
-                                    Return to dashboard
-                                </Button>
-                            </div>
+                        <div>
+                            <GrowthChart />
                         </div>
                     </WidgetCollapsible>
                 </div>
