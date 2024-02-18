630a6c3cdbea2ee18040dc5b3414e76e311df793
henrikmv <henrik@devOtta.no>
2024-02-18 21:38:21 +0100

    feat: changes recommended in review

--- src/Components/GrowthChart/GrowthChart.tsx
+++ src/Components/GrowthChart/GrowthChart.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { GrowthChartBuilder } from "./GrowthChartBuilder";
 import { chartData } from "../../DataSets/ChartData";
+import { useRangeTimePeriode } from './useRangeTimePeriode';
 
 export const GrowthChart = () => {
-    const selectedChart = chartData["Weight-for-age GIRLS"]
+    const { datasets, metadata } = chartData["Weight-for-age GIRLS"];
+    const dataSetValues = datasets["Girls0to5Years"];
+    const dataSetMetadata = metadata["Girls0to5Years"];
 
-    return (
-        <GrowthChartBuilder
-            chartData={selectedChart}
-        />
-    )
-}
+    const xLabelValues = useRangeTimePeriode(dataSetMetadata.range.start, dataSetMetadata.range.end);
+    const keysDataSet = Object.keys(dataSetValues[0]);
+
+    if (xLabelValues.length !== dataSetValues.length) {
+        console.error('xLabelValues and dataSet should have the same length');
+    }
+
+    return <GrowthChartBuilder
+        dataSetValues={dataSetValues}
+        dataSetMetadata={dataSetMetadata}
+        xLabelValues={xLabelValues}
+        keysDataSet={keysDataSet} 
+    />;
+};
--- src/Components/GrowthChart/GrowthChartBuilder/GrowthChartBuilder.tsx
+++ src/Components/GrowthChart/GrowthChartBuilder/GrowthChartBuilder.tsx
@@ -1,64 +1,27 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
-import { ChartData } from '../../../types/chartDataTypes';
-import { useRangeTimePeriode } from './useRangeTimePeriode';
+import { ChartDataTypes } from '../../../types/chartDataTypes';
 
-interface GrowthChartBuilderProps {
-    chartData?: ChartData;
-}
 
-export const GrowthChartBuilder = ({ chartData }: GrowthChartBuilderProps) => {
-    const dataSetName = Object.keys(chartData.datasets)[0];
-    const dataSet = chartData.datasets[dataSetName];
-    const dataSetMetadata = chartData.metadata[dataSetName];
-
-    const xLabelValues = useRangeTimePeriode(dataSetMetadata.range.start, dataSetMetadata.range.end);
-    const keysDataSet = Object.keys(dataSet[0]);
-
-    if ( xLabelValues.length !== dataSet.length ) {
-        console.error('xLabelValues and dataSet should have the same length');
-    }
-
+export const GrowthChartBuilder = ({ dataSetValues, dataSetMetadata, xLabelValues, keysDataSet }: ChartDataTypes) => {
     const data = {
         labels: xLabelValues,
-        datasets: keysDataSet.map((key) => ({
-            data: dataSet.map((entry) => entry[key]),
+        datasets: keysDataSet.map(key => ({
+            data: dataSetValues.map(entry => entry[key]),
             borderWidth: 0.9,
             borderColor: 'black',
         })),
     };
 
     const options = {
-        elements: {
-            point: {
-                radius: 0,
-                hoverRadius: 0,
-            },
-        },
-        plugins: {
-            legend: {
-                display: false,
-            },
-        },
+        elements: { point: { radius: 0, hoverRadius: 0 } },
+        plugins: { legend: { display: false } },
         scales: {
-            x: {
-                title: {
-                    display: true,
-                    text: `age (${dataSetMetadata.unit})`,
-                },
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: dataSetMetadata.yaxis,
-                },
-            },
+            x: { title: { display: true, text: `age (${dataSetMetadata.unit})` } },
+            y: { title: { display: true, text: dataSetMetadata.yaxis } },
         },
     };
 
-    return (
-        <Line data={data} options={options} />
-    );
-
+    return <Line data={data} options={options} />;
 };
