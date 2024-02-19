e0df9f39c37094b1b93cd8180aafe528611033da
petterav <petter@devotta.no>
2024-02-19 16:05:35 +0100

    progress

--- src/Components/GrowthChart/EllipsisButton/EllipsisButton.tsx
+++ src/Components/GrowthChart/EllipsisButton/EllipsisButton.tsx
@@ -1,41 +1,15 @@
 import React from 'react';
-import { IconButton, Menu, MenuItem } from '@material-ui/core';
-// import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
-interface EllipsisButtonProps {
-    handleDelete: () => void;
-}
-
-export const EllipsisButton = ({ handleDelete }: EllipsisButtonProps): JSX.Element => {
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-    const handleOpenMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleCloseMenu = () => {
-        setAnchorEl(null);
-    };
-
+export const EllipsisButton = ({ onClick }: any) => {
     return (
-        <div>
-            <IconButton
-                aria-label="more"
-                aria-controls="ellipsis-button-menu"
-                aria-haspopup="true"
-                onClick={handleOpenMenu}
-            >
-                {/* <MoreHorizIcon /> */}Motherfukcer
-            </IconButton>
-            <Menu
-                id="ellipsis-button-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleCloseMenu}
-            >
-                <MenuItem onClick={handleDelete}>Delete</MenuItem>
-            </Menu>
-        </div>
+        <button
+            type="button"
+            onClick={onClick}
+            className="inline-flex items-center justify-center w-7 h-7 rounded-full text-gray-600 hover:bg-gray-300 hover:text-gray-700 focus:outline-none focus:bg-gray-300 focus:text-gray-700"
+        >
+            <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 10a2 2 0 110 4 2 2 0 010-4zm7 0a2 2 0 110 4 2 2 0 010-4zm7 0a2 2 0 110 4 2 2 0 010-4z" fill="currentColor" fillRule="evenodd" />
+            </svg>
+        </button>
     );
 };
