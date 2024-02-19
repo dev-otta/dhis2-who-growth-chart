cf96689b7d6f4812a8a424af76423c8f228495b9
petterav <136808617+petterav@users.noreply.github.com>
2024-02-19 16:01:55 +0100

    implement tailwind (#6)

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+      extend: {
+          transitionProperty: { width: 'width' },
+          colors: {
+              'dhis-light': '#79B9E3',
+              'dhis-dark': '#3A668F',
+              'dhis-secondary': '#1F78B4',
+          },
+      },
+  },
+  plugins: [
+      // eslint-disable-next-line import/no-extraneous-dependencies,global-require
+      require('@tailwindcss/line-clamp'),
+  ],
+};
