03123d0b5e29d4548011970e8f23ada5fd00780d
edvinstava <136808756+edvinstava@users.noreply.github.com>
2024-02-19 16:04:30 +0100

    feat: [WCG-7] Implement colors for lines in the growth chart (#4)

--- src/Components/GrowthChart/GrowthChartBuilder/GrowthChartBuilder.tsx
+++ src/Components/GrowthChart/GrowthChartBuilder/GrowthChartBuilder.tsx
@@ -3,6 +3,7 @@ import i18n from '@dhis2/d2-i18n'
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 import { ChartDataTypes } from '../../../types/chartDataTypes';
+import { chartLineColorPicker } from '../../../utils/chartLineColorPicker';
 
 
 export const GrowthChartBuilder = ({ dataSetValues, dataSetMetadata, xLabelValues, keysDataSet }: ChartDataTypes) => {
@@ -11,7 +12,7 @@ export const GrowthChartBuilder = ({ dataSetValues, dataSetMetadata, xLabelValue
         datasets: keysDataSet.map(key => ({
             data: dataSetValues.map(entry => entry[key]),
             borderWidth: 0.9,
-            borderColor: 'black',
+            borderColor: chartLineColorPicker(key),
         })),
     };
 
