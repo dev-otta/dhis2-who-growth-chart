5e67a79cccf26b3587a0ca40f9b223c455cc6e6d
edvinstava <edvin@devotta.no>
2024-02-22 20:27:12 +0100

    fix: alterations after review and improved code

--- src/Components/GrowthChart/GrowthChartBuilder/GrowthChartBuilder.tsx
+++ src/Components/GrowthChart/GrowthChartBuilder/GrowthChartBuilder.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import i18n from '@dhis2/d2-i18n';
 import { Line } from 'react-chartjs-2';
-import { Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js';
+import 'chart.js/auto';
 import { ChartDataTypes } from '../../../types/chartDataTypes';
 import { chartLineColorPicker } from '../../../utils/chartLineColorPicker';
-
-Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+import { annotateLineEnd } from '../../../utils/annotateLineEnd';
 
 export const GrowthChartBuilder = ({
     dataSetValues, dataSetMetadata, xLabelValues, keysDataSet,
@@ -20,30 +19,16 @@ export const GrowthChartBuilder = ({
         })),
     };
 
-    const options = {
+    const options: any = {
         elements: { point: { radius: 0, hoverRadius: 0 } },
         plugins: { legend: { display: false } },
         scales: {
             x: { title: { display: true, text: i18n.t(`age (${dataSetMetadata.unit})`) } },
             y: { title: { display: true, text: dataSetMetadata.yaxis } },
         },
-        layout: { padding: { right: 60 } },
+        layout: { padding: { right: 75 } },
+        animation: { onProgress: annotateLineEnd },
     };
-
-    const afterDraw = (chart: Chart<'line', number[], string>) => {
-        const { ctx } = chart;
-        chart.data.datasets.forEach((dataset, index) => {
-            const meta = chart.getDatasetMeta(index);
-            const [lastElement] = meta.data.slice(-1);
-            const { x, y } = lastElement.getProps(['x', 'y']);
-            ctx.fillStyle = typeof dataset.borderColor === 'string' ? dataset.borderColor : 'black';
-            ctx.fillText(dataset.label, x + 10, y);
-        });
-    };
-
-    useEffect(() => {
-        Chart.register({ id: 'custom_draw', afterDraw });
-    }, []);
 
     return <Line data={data} options={options} />;
 };
