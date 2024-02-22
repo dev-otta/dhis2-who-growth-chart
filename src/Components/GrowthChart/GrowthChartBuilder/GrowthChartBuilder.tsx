ac604e387f2dbfa1847561949dbe350021925413
edvinstava <edvin@devotta.no>
2024-02-22 13:31:50 +0100

    feat: Added text at the end of each line showing z-score

--- src/Components/GrowthChart/GrowthChartBuilder/GrowthChartBuilder.tsx
+++ src/Components/GrowthChart/GrowthChartBuilder/GrowthChartBuilder.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import i18n from '@dhis2/d2-i18n';
 import { Line } from 'react-chartjs-2';
-import 'chart.js/auto';
+import { Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js';
 import { ChartDataTypes } from '../../../types/chartDataTypes';
 import { chartLineColorPicker } from '../../../utils/chartLineColorPicker';
+
+Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
 
 export const GrowthChartBuilder = ({
     dataSetValues, dataSetMetadata, xLabelValues, keysDataSet,
@@ -14,6 +16,7 @@ export const GrowthChartBuilder = ({
             data: dataSetValues.map((entry) => entry[key]),
             borderWidth: 0.9,
             borderColor: chartLineColorPicker(key),
+            label: key,
         })),
     };
 
@@ -24,7 +27,29 @@ export const GrowthChartBuilder = ({
             x: { title: { display: true, text: i18n.t(`age (${dataSetMetadata.unit})`) } },
             y: { title: { display: true, text: dataSetMetadata.yaxis } },
         },
+        layout: { padding: { right: 60 } },
     };
+
+    useEffect(() => {
+        Chart.register({
+            id: 'custom_draw',
+            afterDraw: (chart) => {
+                const { ctx } = chart;
+                ctx.save();
+                ctx.font = `${Chart.defaults.font.size}px ${Chart.defaults.font.family}`;
+                ctx.fillStyle = 'red';
+                ctx.textBaseline = 'bottom';
+                chart.data.datasets.forEach((dataset, index) => {
+                    const meta = chart.getDatasetMeta(index);
+                    const [lastElement] = meta.data.slice(-1);
+                    const { x, y } = lastElement.getProps(['x', 'y']);
+                    ctx.fillStyle = typeof dataset.borderColor === 'string' ? dataset.borderColor : 'black';
+                    ctx.fillText(dataset.label, x + 10, y);
+                });
+                ctx.restore();
+            },
+        });
+    }, []);
 
     return <Line data={data} options={options} />;
 };
