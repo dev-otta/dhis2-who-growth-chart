8346b5654c17ec689fc6ff01eb60574b508d1d14
henrikmv <henrik@devOtta.no>
2024-02-26 15:47:11 +0100

    feat: add z-score values

--- src/utils/annotateLineEnd.ts
+++ src/utils/annotateLineEnd.ts
@@ -9,17 +9,35 @@ interface DataSet {
 
 export const annotateLineEnd = (animation: Animation & { chart?: Chart }) => {
     const { chart } = animation;
-    if (!chart) {
-        return;
-    }
+    if (!chart) return;
+
     const { ctx } = chart;
+
+    const extractNumberFromLabel = (label: string): number => {
+        const numberStr = label.replace(/[^\d.-]/g, '');
+        return parseFloat(numberStr);
+    };
+
+    const adjustLabel = (labelText: number, originalLabel: string): string => {
+        const isNegative = originalLabel.includes('neg');
+        if (isNegative) return `-${labelText}`;
+        return ` ${labelText.toString()}`;
+    };
+
+    const drawText = (ctx: CanvasRenderingContext2D, text: string, x: number, y: number, color: string) => {
+        ctx.fillStyle = color;
+        ctx.font = '14px Arial';
+        ctx.fillText(text, x, y);
+    };
 
     chart.data.datasets.forEach((dataset: DataSet, index: number) => {
         const meta = chart.getDatasetMeta(index);
         const [lastElement] = meta.data.slice(-1);
         const { x, y } = lastElement.getProps(['x', 'y']);
-        ctx.fillStyle = dataset.borderColor;
-        ctx.font = '14px Arial';
-        ctx.fillText(dataset.label, x + 10, y);
+
+        const labelText = extractNumberFromLabel(dataset.label);
+        const adjustedLabelText = adjustLabel(labelText, dataset.label);
+
+        drawText(ctx, adjustedLabelText, x + 3, y + 4, dataset.borderColor);
     });
 };
