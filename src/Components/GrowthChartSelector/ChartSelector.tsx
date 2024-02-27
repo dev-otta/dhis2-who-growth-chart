908173ee1caea26d8c10d333cf2d2652a0bc2b99
edvinstava <edvin@devotta.no>
2024-02-27 21:06:36 +0100

    feat: Added headlessui and improved css

--- src/Components/GrowthChartSelector/ChartSelector.tsx
+++ src/Components/GrowthChartSelector/ChartSelector.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Menu } from '@headlessui/react';
 import { CategoryCodes } from '../../types/chartDataTypes';
 import { chartData } from '../../DataSets/WhoStandardDataSets/ZScores/ChartDataZscores';
 
@@ -15,34 +16,75 @@ export const ChartSelector = ({
     setCategory,
     setDataset,
 }: ChartSelectorProps) => {
-    const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const newCategory = CategoryCodes[event.target.value as keyof typeof CategoryCodes];
+    const handleCategoryChange = (key: string) => {
+        const newCategory = CategoryCodes[key as keyof typeof CategoryCodes];
         setCategory(newCategory);
         setDataset(Object.keys(chartData[newCategory].datasets)[0]);
     };
 
-    const handleDatasetChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const newDataset = event.target.value;
+    const handleDatasetChange = (key: string) => {
+        const newDataset = key as keyof typeof chartData;
         setDataset(newDataset);
     };
 
     return (
-        <div>
-            <select value={CategoryCodes[category]} onChange={handleCategoryChange}>
-                {Object.keys(CategoryCodes).map((key) => (
-                    <option key={key} value={key}>
-                        {CategoryCodes[key as keyof typeof CategoryCodes]}
-                    </option>
-                ))}
-            </select>
-
-            <select value={dataset} onChange={handleDatasetChange}>
-                {Object.keys(chartData[category].datasets).map((key) => (
-                    <option key={key} value={key}>
-                        {key}
-                    </option>
-                ))}
-            </select>
+        <div className='w-full flex justify-center items-center'>
+            <div className='flex w-1/2 gap-2 py-2 font-medium'>
+                <div className='flex w-1/2 relative'>
+                    <Menu>
+                        <Menu.Button
+                            className='py-3 bg-gray-200 rounded w-full h-full'
+                        >
+                            {category}
+                        </Menu.Button>
+                        <Menu.Items className='flex flex-col w-full bg-white rounded shadow-lg absolute'>
+                            {Object.keys(CategoryCodes).map((key) => (
+                                <Menu.Item key={key}>
+                                    {({ active }) => (
+                                        <button
+                                            className={
+                                                `
+                                                ${active && 'bg-gray-300'} 
+                                                ${CategoryCodes[key as keyof typeof CategoryCodes] === category
+                                                && 'bg-gray-200'} 
+                                                py-3`
+                                            }
+                                            onClick={() => handleCategoryChange(key)}
+                                        >
+                                            {CategoryCodes[key as keyof typeof CategoryCodes]}
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            ))}
+                        </Menu.Items>
+                    </Menu>
+                </div>
+                <div className='flex w-1/2 relative'>
+                    <Menu>
+                        <Menu.Button
+                            className='py-3 bg-gray-200 rounded w-full h-full'
+                        >
+                            {dataset}
+                        </Menu.Button>
+                        <Menu.Items className='flex flex-col w-full bg-white rounded shadow-lg absolute'>
+                            {Object.keys(chartData[category].datasets).map((key) => (
+                                <Menu.Item key={key}>
+                                    {({ active }) => (
+                                        <button
+                                            className={
+                                                `${active && 'bg-gray-300'} ${key === dataset && 'bg-gray-200'} py-3`
+                                            }
+                                            onClick={() => handleDatasetChange(key)}
+                                        >
+                                            {key}
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            ))}
+                        </Menu.Items>
+                    </Menu>
+                </div>
+            </div>
         </div>
     );
 };
