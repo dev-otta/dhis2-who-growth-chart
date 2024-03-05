3f5671969da774709b0db23d7cf306456ea02b88
petterav <petter@devotta.no>
2024-03-05 14:20:07 +0100

    fix: good fix, no warnings

--- src/UI/Icons/EllipsisIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const EllipsisIcon = ({ className }: { className: string }) => (
-    <svg height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg' className={className}>
-        {/* eslint-disable-next-line max-len */}
-        <path d='M5 10a2 2 0 110 4 2 2 0 010-4zm7 0a2 2 0 110 4 2 2 0 010-4zm7 0a2 2 0 110 4 2 2 0 010-4z' fill='currentColor' fillRule='evenodd' />
-    </svg>
-);
