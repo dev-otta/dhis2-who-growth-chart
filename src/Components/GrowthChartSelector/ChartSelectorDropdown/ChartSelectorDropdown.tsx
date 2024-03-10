bce56c686757f539879f5080c2d28c9ab0d38994
petterav <136808617+petterav@users.noreply.github.com>
2024-03-10 16:01:36 +0100

    feat: [CGC-17] Filestructure and Import SVGs (#26)
    Co-authored-by: henrikmv <henrik@devOtta.no>
    Co-authored-by: edvinstava <edvin@devotta.no>

--- src/Components/GrowthChartSelector/ChartSelectorDropdown/ChartSelectorDropdown.tsx
+++ src/Components/GrowthChartSelector/ChartSelectorDropdown/ChartSelectorDropdown.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Menu } from '@headlessui/react';
 import { CategoryCodes, ChartData } from '../../../types/chartDataTypes';
+import { Chevron } from '../../../UI/Icons';
 
 interface ChartSelectorDropdownProps {
     title: keyof typeof CategoryCodes | keyof ChartData;
@@ -31,17 +32,8 @@ export const ChartSelectorDropdown = ({
                             className='flex flex-row rounded border border-gray-300 py-1 gap-2 h-7 pl-4 pr-2 items-center whitespace-nowrap'
                         >
                             {title}
-                            <svg
-                                xmlns='http://www.w3.org/2000/svg'
-                                viewBox='0 0 12 12'
-                                className={`w-3 h-3 ${open ? 'rotate-180' : ''}`}
-                            >
-                                <path
-                                    d={`m5.29289 8.7071c.39053.3905 1.02369.3905 1.41422 0l2.99999-2.99999c.3905-.39053.3905-1.02369 
-                                    0-1.41422-.3905-.39052-1.0237-.39052-1.4142 0l-2.2929 2.2929-2.29289-2.2929c-.39053-.39052-1.02369-.39052-1.41422 
-                                    0-.39052.39053-.39052 1.02369 0 1.41422z`}
-                                />
-                            </svg>
+                            <Chevron className={`w-3 h-3 ${open ? 'rotate-180' : ''}`} />
+
                         </Menu.Button>
                         <Menu.Items className='flex flex-col bg-white rounded shadow-lg z-10 absolute mt-8'>
                             {items.map((key) => (
