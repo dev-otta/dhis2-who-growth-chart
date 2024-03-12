9af102ff48a485b1a355a12e2d6ce00ec90241c7
edvinstava <edvin@devotta.no>
2024-03-12 14:23:29 +0100

    fix: fixed lint error

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  video: false,
+    video: false,
 
-  component: {
-    setupNodeEvents() {},
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+    component: {
+        setupNodeEvents() {},
+        devServer: {
+            framework: 'create-react-app',
+            bundler: 'webpack',
+        },
     },
-  },
 
-  e2e: {
-    setupNodeEvents(on, config) {
+    e2e: {
+        setupNodeEvents() {
       // implement node event listeners here
+        },
     },
-  },
 });
