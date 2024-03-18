49c37c61885bd6b63c17313d5d11cebd193d4b87
petterav <petter@devotta.no>
2024-03-18 12:34:56 +0100

    fix: type error

--- src/DataSets/WhoStandardDataSets/Percentiles/lhfa-girls/index.ts
+++ src/DataSets/WhoStandardDataSets/Percentiles/lhfa-girls/index.ts
@@ -1,3 +1,3 @@
 export { default as lhfa_g_0_2_y_p } from './lhfa_girls_0-to-2-years_percentiles.json';
-export { default as lhfa_g_0_13_w_p } from './lhfa_girls_0-to-13-years_percentiles.json';
+export { default as lhfa_g_0_13_w_p } from './lhfa_girls_0-to-13-weeks_percentiles.json';
 export { default as lhfa_g_2_5_y_p } from './lhfa_girls_2-to-5-years_percentiles.json';
