8681f2cb95b2396603c15ffbaf5489237454faea
petterav <petter@devotta.no>
2024-03-20 15:13:26 +0100

    fix: tsc error

--- src/Components/GrowthChart/GrowthChartBuilder/tooltipConfig.ts
+++ src/Components/GrowthChart/GrowthChartBuilder/tooltipConfig.ts
@@ -22,36 +22,32 @@ interface TooltipConfig {
     };
 }
 
-export const tooltipConfig = (formattedFieldName: string, category: string): TooltipConfig => {
-    console.log('formattedFieldName', formattedFieldName);
-    console.log('category', category);
-    return {
-        enabled: true,
-        intersect: false,
-        position: 'nearest' as Scriptable<keyof TooltipPositionerMap, ScriptableTooltipContext<'line'>>,
-        backgroundColor: 'white',
-        bodyFont: { size: 12 },
-        bodyColor: 'black',
-        borderColor: 'black',
-        borderWidth: 1,
-        padding: 12,
-        caretPadding: 4,
-        boxPadding: 4,
-        usePointStyle: true,
-        filter: (tooltipItem) => tooltipItem.dataset.id === 'measurementData',
-        callbacks: {
-            title: () => '',
-            beforeLabel: (tooltipItem) => {
-                const date = new Date(tooltipItem.raw.eventDate).toLocaleDateString();
-                return `${i18n.t('Date')}: ${date}`;
-            },
-            label: (tooltipItem) => {
-                if (category === 'wflh_b' || category === 'wflh_g') {
-                    return `${i18n.t('Height')}: ${tooltipItem.label} | ${i18n.t('Weight')}: ${tooltipItem.formattedValue}`;
-                }
-                const value = tooltipItem.formattedValue;
-                return `${formattedFieldName}: ${value}`;
-            },
+export const tooltipConfig = (formattedFieldName: string, category: string): TooltipConfig => ({
+    enabled: true,
+    intersect: false,
+    position: 'nearest' as Scriptable<keyof TooltipPositionerMap, ScriptableTooltipContext<'line'>>,
+    backgroundColor: 'white',
+    bodyFont: { size: 12 },
+    bodyColor: 'black',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 12,
+    caretPadding: 4,
+    boxPadding: 4,
+    usePointStyle: true,
+    filter: (tooltipItem) => tooltipItem.dataset.id === 'measurementData',
+    callbacks: {
+        title: () => '',
+        beforeLabel: (tooltipItem) => {
+            const date = new Date(tooltipItem.raw.eventDate).toLocaleDateString();
+            return `${i18n.t('Date')}: ${date}`;
         },
-    };
-};
+        label: (tooltipItem) => {
+            if (category === 'wflh_b' || category === 'wflh_g') {
+                return `${i18n.t('Height')}: ${tooltipItem.label} | ${i18n.t('Weight')}: ${tooltipItem.formattedValue}`;
+            }
+            const value = tooltipItem.formattedValue;
+            return `${formattedFieldName}: ${value}`;
+        },
+    },
+});
