66d587b772612e803081976d1724e01313beb4fb
henrikmv <henrik@devOtta.no>
2024-06-17 15:51:32 +0200

    fix: define props type with ButtonProps

--- src/components/GrowthChart/ChartSettingsButton/EllipsisButton/EllipsisButton.tsx
+++ src/components/GrowthChart/ChartSettingsButton/EllipsisButton/EllipsisButton.tsx
@@ -1,28 +1,19 @@
 import React, { useState, useRef } from 'react';
-import { Button, Layer, Popper, FlyoutMenu, IconMore16, MenuItem } from '@dhis2/ui';
+import { Button, Layer, Popper, FlyoutMenu, IconMore16, MenuItem, ButtonProps } from '@dhis2/ui';
 
-type Props = {
-    label?: string,
-    primary?: boolean,
-    secondary?: boolean,
-    icon?: React.ReactElement,
-    onClick?: () => void,
-    dataTest?: string,
-    small?: boolean,
-    large?: boolean,
-};
+interface Props extends ButtonProps {
+    label?: string;
+    icon?: React.ReactElement;
+    onClick: () => void;
+}
 
 export const EllipsisButton = ({
     label,
-    primary,
-    secondary,
-    small,
-    large,
-    onClick: handleClick,
     icon,
-    dataTest,
+    onClick,
+    ...buttonProps
 }: Props) => {
-    const anchorRef = useRef(null);
+    const anchorRef = useRef<HTMLDivElement>(null);
     const [actionsIsOpen, setActionsIsOpen] = useState(false);
 
     const toggle = () => {
@@ -32,15 +23,10 @@ export const EllipsisButton = ({
     return (
         <>
             <div ref={anchorRef}>
-                {/* @ts-ignore */}
                 <Button
-                    primary={primary}
-                    secondary={secondary}
-                    dataTest={dataTest}
-                    small={small}
-                    large={large}
                     onClick={toggle}
-                    icon={<IconMore16 />}
+                    icon={icon ?? <IconMore16 />}
+                    {...buttonProps}
                 />
             </div>
             {actionsIsOpen && (
@@ -49,7 +35,7 @@ export const EllipsisButton = ({
                         <FlyoutMenu dense>
                             <MenuItem
                                 label={label}
-                                onClick={handleClick}
+                                onClick={onClick}
                                 icon={icon}
                             />
                         </FlyoutMenu>
